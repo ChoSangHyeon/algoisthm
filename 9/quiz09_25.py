2804a689833c846e229506f52f5989c7a15178da
ChoSangHyeon <whtkdgusdldi@naver.com>
2022-01-18 16:00:55 +0900

    1-18-3

--- 9/quiz09_25.py
+++ 9/quiz09_25.py
@@ -16,4 +16,50 @@
 # circularQueue.enQueue(60);    // true 리턴
 # circularQueue.Rear();         // 60
 # circularQueue.Front();        // 30 리턴
+class Node:
+    def __init__(self, item,next):
+        self.item = item
+        self.next = next
+
+class circularQueue:
+    def __init__(self):
+        self.head = None
+
+    def MyCircularQueue(self,int):
+        node = Node(self, None)
+        self.head = None
+        self.head.next  = node
+        while int>0:
+            node.next = node
+            int -=1
+        node.next = self.head
+        return
+    def enQueueu(self,val):
+        node = self.head
+        node.item = val
+        for i in range(len(node)-1):
+            node = node.next
+        node.next = self.head
+
+    def deQueue(self):
+        node = self.head
+        node.item = None
+        for i in range(len(node) - 1):
+            node = node.next
+        node.next = self.head
+
+
+    def Rear(self):
+        node = self.head
+        for i in range(len(node) - 1):
+            node = node.next
+        return node.item
+
+    def Front(self):
+        node = self.head
+        node = node.next
+        return node.item
+
+    def isFull(self):
+        
 
